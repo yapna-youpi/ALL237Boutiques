c85e4b68a58fef8a097b9a18243313f66d772c91
Folley-05 <aristotepascaldjounda@gmail.com>
2023-01-06 16:00:23 +0100

    fix input issue

--- src/components/addons/input/PhoneInput.jsx
+++ src/components/addons/input/PhoneInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PhoneInput from 'react-phone-number-input';
-import Tooltip,{ tooltipClasses } from '@material-ui/core/Tooltip';
+import Tooltip, { tooltipClasses } from '@material-ui/core/Tooltip';
 import { IoAlertCircleOutline } from 'react-icons/io5'
 import { makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
@@ -12,22 +12,22 @@ import './phone-input.css'
 
 const useStyles = makeStyles((theme) => ({
     span: {
-      margin: theme.spacing(1),
+        margin: theme.spacing(1),
     },
     customWidth: {
-      maxWidth: 100,
+        maxWidth: 100,
     },
     noMaxWidth: {
-      maxWidth: 250,
+        maxWidth: 250,
     },
 }));
 
-function InputPhone({label, name, val, id, error, help, change, handBlur, country, all, cm,alert }) {
+function InputPhone({ label, name, val, id, error, help, change, handBlur, country, all, cm, alert }) {
     const { t } = useTranslation();
     // console.log("the country ", country) for tooltip of country which services are unavalable
     const classes = useStyles();
-    const longText =`${t('tooltip')}` 
-     
+    const longText = `${t('tooltip')}`
+
     return (
         <div className="input-phone">
             <label htmlFor=""> {label} </label>
@@ -36,21 +36,20 @@ function InputPhone({label, name, val, id, error, help, change, handBlur, countr
                     <PhoneInput name={name} id={id}
                         // countries={['CM', 'SN', 'BF', 'CI', 'ML', 'GN']}
                         value={val}
-                        countries={!all ? (cm ? ['CM'] : ['CM', 'SN', 'CI']): null}
+                        countries={!all ? (cm ? ['CM'] : ['CM', 'SN', 'CI']) : null}
                         // countries={null}
                         defaultCountry={country || 'CM'} international countryCallingCodeEditable={false}
-                        onChange={(value)=>change(name, value)}
-                        onBlur={ handBlur ? (e)=>handBlur(e.target) : ()=>{}}
+                        onChange={(value) => change(name, value)}
+                        onBlur={handBlur ? (e) => handBlur(e.target) : () => { }}
                     />
                 </div>
                 <div className="tooltip-input">
-                    { alert ?   (<Box ><Tooltip  title={longText}  placement="top" arrow>
-                                    <span className={classes.button}><IoAlertCircleOutline size={25} color="#f39c12" /></span>
-                                </Tooltip></Box>) : '' }
-                    
+                    {alert ? (<Box ><Tooltip title={longText} placement="top" arrow>
+                        <span className={classes.button}><IoAlertCircleOutline size={25} color="#f39c12" /></span>
+                    </Tooltip></Box>) : ''}
                 </div>
             </div>
-            <span>{error&&help}</span>
+            <span>{error && help}</span>
         </div>
     )
 }
